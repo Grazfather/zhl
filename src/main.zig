b5df66ec8175c666c9a505c6742fd95ec34e07bb
Grazfather <grazfather@gmail.com>
2024-11-08 11:43:23 -0500

    Optimize

--- src/main.zig
+++ src/main.zig
@@ -3,81 +3,71 @@ const Allocator = std.mem.Allocator;
 const mvzr = @import("mvzr.zig");
 const Crc = std.hash.crc.Crc16DectX;
 
-const ansi_escape = "\u{001b}";
-const ansi_color_start = ansi_escape ++ "[38;5;{d}m";
+const ansi_escape = "\x1b";
+const ansi_color_start = ansi_escape ++ "[38;5;{}m";
 const ansi_color_end = ansi_escape ++ "[0m";
 const colorize_fmt_string = std.fmt.comptimePrint("{s}{{s}}{s}", .{ ansi_color_start, ansi_color_end });
 
-fn colorize(allocator: Allocator, s: []const u8) ![]const u8 {
-    const color = get_color(s);
-    return std.fmt.allocPrint(allocator, colorize_fmt_string, .{ color, s });
-}
-
-fn colorize_line(allocator: Allocator, s: []const u8, match_it: *mvzr.Regex.RegexIterator) ![]const u8 {
+fn colorize_line(allocator: Allocator, line: []const u8, regex: *const mvzr.Regex) ![]const u8 {
     var start: usize = 0;
-    var line = try std.ArrayList(u8).initCapacity(allocator, s.len);
+    var output = try std.ArrayList(u8).initCapacity(allocator, line.len);
 
-    while (match_it.next()) |m| {
-        // Color the current match
-        const colorized_match = try colorize(allocator, m.slice);
+    var it = regex.iterator(line);
+    while (it.next()) |match| {
+        // Get the color for the current match
+        const color = get_color(match.slice);
         // Append everything from the end of the last match to the start of the current
-        try line.appendSlice(s[start..m.start]);
+        try output.appendSlice(line[start..match.start]);
         // Append the colorized match
-        try line.appendSlice(colorized_match);
-        allocator.free(colorized_match);
+        try output.writer().print(ansi_color_start, .{color});
+        try output.appendSlice(match.slice);
+        try output.appendSlice(ansi_color_end);
         // Update the cursor to the end of the match
-        start = m.end;
+        start = match.end;
     }
 
     // Append everything after the last match
-    try line.appendSlice(s[start..]);
-    return line.items;
+    try output.appendSlice(line[start..]);
+    return output.items;
 }
 
 fn get_color(s: []const u8) u8 {
-    const hash = Crc.hash(s);
-    // TODO: Lookup in a comptime colour table
+    const hash = std.hash.crc.Crc16DectX.hash(s);
     return @as(u8, @truncate(hash)) % 200 + 16;
 }
 
 pub fn main() !void {
-    // TODO: Find an appropriate allocator
-    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // const allocator = gpa.allocator();
-    const allocator = std.heap.page_allocator;
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
     const stdout = std.io.getStdOut().writer();
+    var br = std.io.bufferedReader(std.io.getStdIn().reader());
+    var stdin = br.reader();
 
     // Get pattern from command line
     const args = try std.process.argsAlloc(allocator);
-    defer allocator.free(args);
+    // Not really needed with the arena deinit handles this
+    defer std.process.argsFree(allocator, args);
 
     if (args.len != 2) {
         std.debug.print("Usage: zhl PATTERN\n", .{});
         std.process.exit(1);
     }
-
     const pattern = args[1];
-    var br = std.io.bufferedReader(std.io.getStdIn().reader());
-    var stdin = br.reader();
 
     const regex: mvzr.Regex = mvzr.compile(pattern) orelse {
         std.debug.print("Cannot parse regex pattern '{s}'\n", .{pattern});
         std.process.exit(1);
     };
 
-    const buf = try allocator.alloc(u8, 4096);
+    // Allocate enough to read a whole line
+    var buf: [16 * 1024]u8 = undefined;
 
-    (read_loop: {
-        while (stdin.readUntilDelimiterOrEof(buf, '\n') catch |e|
-            break :read_loop e) |line|
-        {
-            var it = regex.iterator(line);
-            const colorized_line = try colorize_line(allocator, line, &it);
-            _ = try stdout.print("{s}\n", .{colorized_line});
-            allocator.free(colorized_line);
-        }
-    }) catch |err| {
-        std.debug.print("Got error: {any}\n", .{err});
-        std.process.exit(1);
-    };
+    while (try stdin.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        const colorized_line = try colorize_line(allocator, line, &regex);
+        _ = try stdout.print("{s}\n", .{colorized_line});
+        // allocator.free(colorized_line);
+        _ = arena.reset(.{ .retain_with_limit = 1 });
+    }
 }
