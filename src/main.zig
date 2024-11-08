1503667ffd86d3c0dc6c73c953fe6aed60162434
Grazfather <grazfather@gmail.com>
2024-11-08 12:05:36 -0500

    Add clap to support default patterns on cli

--- src/main.zig
+++ src/main.zig
@@ -2,18 +2,21 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const mvzr = @import("mvzr.zig");
 const Crc = std.hash.crc.Crc16DectX;
+const clap = @import("clap");
 
 const ansi_escape = "\x1b";
 const ansi_color_start = ansi_escape ++ "[38;5;{}m";
 const ansi_color_end = ansi_escape ++ "[0m";
 const colorize_fmt_string = std.fmt.comptimePrint("{s}{{s}}{s}", .{ ansi_color_start, ansi_color_end });
 
-fn colorize_line(allocator: Allocator, line: []const u8, regex: *const mvzr.Regex) ![]const u8 {
+fn colorize_line(allocator: Allocator, line: []const u8, regex: *const mvzr.Regex, grep: bool) !?[]const u8 {
     var start: usize = 0;
     var output = try std.ArrayList(u8).initCapacity(allocator, line.len);
 
+    var matched = false;
     var it = regex.iterator(line);
     while (it.next()) |match| {
+        matched = true;
         // Get the color for the current match
         const color = get_color(match.slice);
         // Append everything from the end of the last match to the start of the current
@@ -24,6 +27,12 @@ fn colorize_line(allocator: Allocator, line: []const u8, regex: *const mvzr.Rege
         try output.appendSlice(ansi_color_end);
         // Update the cursor to the end of the match
         start = match.end;
+    }
+
+    // If we are grepping, but this line doesn't have a match, then we are done
+    if (!matched and grep) {
+        allocator.free(output.items);
+        return undefined;
     }
 
     // Append everything after the last match
@@ -45,16 +54,51 @@ pub fn main() !void {
     var br = std.io.bufferedReader(std.io.getStdIn().reader());
     var stdin = br.reader();
 
-    // Get pattern from command line
-    const args = try std.process.argsAlloc(allocator);
-    // Not really needed with the arena deinit handles this
-    defer std.process.argsFree(allocator, args);
+    const params = comptime clap.parseParamsComptime(
+        \\-h, --help             Display this help and exit.
+        \\-p, --pattern <str>    Regex pattern to highlight
+        \\-d, --decimalnumbers   Highlight decimal digits
+        \\-w, --words            Highlight (regex) words
+        \\-x, --hexnumbers       Highlight hex numbers
+        \\-g, --grep             Only print matching lines
+        \\
+    );
 
-    if (args.len != 2) {
-        std.debug.print("Usage: zhl PATTERN\n", .{});
-        std.process.exit(1);
+    var gpa_state = std.heap.GeneralPurposeAllocator(.{}){};
+    const gpa = gpa_state.allocator();
+    defer _ = gpa_state.deinit();
+    var diag = clap.Diagnostic{};
+    var res = clap.parse(clap.Help, &params, clap.parsers.default, .{
+        .diagnostic = &diag,
+        .allocator = gpa,
+    }) catch |err| {
+        try diag.report(std.io.getStdErr().writer(), err);
+        try clap.usage(std.io.getStdErr().writer(), clap.Help, &params);
+        return err;
+    };
+    defer res.deinit();
+
+    if (res.args.help != 0) {
+        return clap.usage(std.io.getStdErr().writer(), clap.Help, &params);
     }
-    const pattern = args[1];
+
+    var grep = false;
+    var pattern: []const u8 = undefined;
+    if (res.args.pattern) |p| {
+        pattern = p;
+    } else if (res.args.decimalnumbers != 0) {
+        pattern = "\\b\\d+\\b";
+    } else if (res.args.words != 0) {
+        pattern = "\\w+";
+    } else if (res.args.hexnumbers != 0) {
+        pattern = "0x[a-fA-F0-9]{2,}]";
+    } else {
+        return clap.usage(std.io.getStdErr().writer(), clap.Help, &params);
+    }
+
+    if (res.args.grep == 1) {
+        grep = true;
+    }
 
     const regex: mvzr.Regex = mvzr.compile(pattern) orelse {
         std.debug.print("Cannot parse regex pattern '{s}'\n", .{pattern});
@@ -65,9 +109,10 @@ pub fn main() !void {
     var buf: [16 * 1024]u8 = undefined;
 
     while (try stdin.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        const colorized_line = try colorize_line(allocator, line, &regex);
-        _ = try stdout.print("{s}\n", .{colorized_line});
-        // allocator.free(colorized_line);
+        const colorized_line = try colorize_line(allocator, line, &regex, grep);
+        if (colorized_line) |cl| {
+            _ = try stdout.print("{s}\n", .{cl});
+        }
         _ = arena.reset(.{ .retain_with_limit = 1 });
     }
 }
