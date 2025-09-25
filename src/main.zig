767e85bbf65c0397e98b330cf1629f7203d63107
Grazfather <grazfather@gmail.com>
2025-09-25 10:19:15 -0400

    Avoid string formatting in hot path

--- src/main.zig
+++ src/main.zig
@@ -68,7 +68,11 @@ fn colorize_line(
         if (!matches_only)
             try output.appendSlice(line[start..match.start]);
         // Append the colorized match
-        try output.writer().print(ansi_color_start, .{color});
+        try output.appendSlice("\x1b[38;5;");
+        var buf: [3]u8 = undefined; // u8 max is 255, so max 3 digits
+        const len = std.fmt.formatIntBuf(&buf, color, 10, .lower, .{});
+        try output.appendSlice(buf[0..len]);
+        try output.appendSlice("m");
         try output.appendSlice(match.slice);
         try output.appendSlice(ansi_color_end);
         // Update the cursor to the end of the match
